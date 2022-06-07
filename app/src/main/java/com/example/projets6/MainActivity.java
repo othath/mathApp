619af0cb1697fc35452d8b18bcc8858b4bca7699
Max Aumard <max.aumard93@gmail.com>
2022-06-08 00:22:18 +0200

    implémentation design : main menu + début connexion + début inscription + début mode classic

--- app/src/main/java/com/example/projets6/MainActivity.java
+++ app/src/main/java/com/example/projets6/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.projets6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.projets6.back.logInController;
 
@@ -12,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
     }
 }
