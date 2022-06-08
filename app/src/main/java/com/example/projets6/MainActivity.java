35a590c0875a03fe5ebeab0e2adcbccfc4a256ee
Othmane <othmanetahri77@gmail.com>
2022-06-08 17:12:09 +0200

    Link Log In Interface

--- app/src/main/java/com/example/projets6/MainActivity.java
+++ app/src/main/java/com/example/projets6/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.projets6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
+import com.example.projets6.activity.logInActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,7 +18,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        Button test=(Button) findViewById(R.id.test);
+        test.setOnClickListener(v -> login());
+    }
+    public void login(){
+        Intent intent=new Intent(this,logInActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/projets6/activity/logInActivity.java
+++ app/src/main/java/com/example/projets6/activity/logInActivity.java
@@ -2,8 +2,12 @@ package com.example.projets6.activity;
 
 
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -28,61 +32,69 @@ public class logInActivity extends AppCompatActivity {
     private EditText userField;
     private EditText passwordField;//en XML  android:inputType="Password"
     private EditText confirmPasswordField;
-    public Pattern p = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+   // public Pattern p = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
     //Pattern.compile("^(.+)@(.+)$");
+    public Pattern p=Pattern.compile("^[A-Z]+[A-Z0-9._-]+");
     private TextView userMessage;
-    private TextView signupMessage;
-    private Button signInOrUp;
+    private TextView signUpMessage;
     private EditText signUpName;
-    private EditText signUpEmail;
+    private EditText signUpUser;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        loginBtn=findViewById(R.id.logInBtn);
-        userField=findViewById(R.id.userField);
-        passwordField=findViewById(R.id.passwordField);
+        setContentView(R.layout.activity_connexion);
+
+        loginBtn=(Button)findViewById(R.id.logInBtn);
+        userField=(EditText) findViewById(R.id.userField);
+        passwordField=(EditText) findViewById(R.id.passwordField);
        // confirmPasswordField=findViewById(R.id.confirmPasswordField);
-        userMessage=findViewById(R.id.userMessage);
-        //signupMessage=findViewById(R.id.signupMessage);
-
-
+        userMessage=(TextView) findViewById(R.id.userMessage);
+        //signInMessage=findViewById(R.id.signInMessage);
+        //signInUser=findViewById(R.id.)
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                connectBtn(v);
+                Log.i("DEBUG","clicked");
+            }
+        });
 
 
 
     }
-
-
-
-    private  String passEncrypted=getSHA(passwordField.getText().toString());
-    private String confirmPassEncrypted=getSHA(confirmPasswordField.getText().toString());
+    //private  String passEncrypted=getSHA(passwordField.getText().toString());
+    //private String confirmPassEncrypted=getSHA(confirmPasswordField.getText().toString());
+    private String passEncrypted=getSHA("TEST");
+    private String confirmPassEncrypted=getSHA("ee");
     public boolean loginFormIsValid() {
         if (!p.matcher(userField.getText()).matches()) {
             userMessage.setText("Wrong !");
             return false;
         } else {
-            userMessage.setText("");
+            userMessage.setText("true");
             return true;
         }
     }
 
-    public boolean signupFormIsValid() {
+/*    public boolean signupFormIsValid() {
         if (!p.matcher(userField.getText()).matches()) {
-            signupMessage.setText("Wrong email !");
+            signUpMessage.setText("Wrong email !");
             return false;
         } else if (!passEncrypted.equals(confirmPassEncrypted)) {
-            signupMessage.setText("Passwords don't match");
+            signUpMessage.setText("Passwords don't match");
             return false;
         } else {
-            signupMessage.setText("");
+            signUpMessage.setText("");
             return true;
         }
-    }
+    }*/
 
-    public void ConnectBtn() {
+    public void connectBtn(View view) {
         DbConnector connect = new DbConnector();
         Connection connectDB = connect.getConnection();
-
+        Log.i("DEBUG","on fucntion");
         if (loginFormIsValid()) {
             try {
                 String connectQuery = "SELECT id,name,email, password FROM player WHERE email='" + userField.getText() + "' and password='" + passEncrypted + "';";
@@ -100,23 +112,23 @@ public class logInActivity extends AppCompatActivity {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-        } else if(signupFormIsValid()) {
+        }
+        /*else if(signupFormIsValid()) {
             try{
                 String connectQuery = "select count(id) from player;";
                 Statement statement = connectDB.createStatement();
                 ResultSet queryOutput = statement.executeQuery(connectQuery);
                 queryOutput.next();
-                int current_id = queryOutput.getInt(1) + 1;
+                //int current_id = queryOutput.getInt(1) + 1;
                 String name =signUpName.getText().toString();
-                String createQuery = String.format("INSERT INTO client VALUES(%d,'%s','%s')",current_id,name, userField.getText(), passEncrypted);
+                String createQuery = String.format("INSERT INTO player(userName,password) VALUES('%s','%s')",userField.getText(), passEncrypted);
                 statement.executeUpdate(createQuery);
-                signupMessage.setText("Account created successfully !");
+                signUpMessage.setText("Account created successfully !");
             } catch (Exception e) {
                 e.printStackTrace();
-            }
+            }*/
         }
 
-    }
 
     public static String getSHA(String input) {
 
@@ -151,6 +163,7 @@ public class logInActivity extends AppCompatActivity {
             return null;
         }
     }
+
 
 }
 
