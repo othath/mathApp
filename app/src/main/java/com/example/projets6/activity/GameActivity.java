b4d9508984e7ec81b7467b0d38a9ac0c9cbde7fd
Max Aumard <max.aumard93@gmail.com>
2022-06-08 15:15:59 +0200

    responsive layers

---
+++ app/src/main/java/com/example/projets6/activity/GameActivity.java
@@ -0,0 +1,21 @@
+package com.example.projets6.activity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+import com.example.projets6.R;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.classicmode);
+
+    }
+}
