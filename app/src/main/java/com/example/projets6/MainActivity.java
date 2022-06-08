ef87ccc3c78a29648768d7341163bcfa656ea0d0
Max Aumard <max.aumard93@gmail.com>
2022-06-08 19:56:02 +0200

    settings

--- app/src/main/java/com/example/projets6/MainActivity.java
+++ app/src/main/java/com/example/projets6/MainActivity.java
@@ -8,11 +8,14 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageButton;
 
+import com.example.projets6.activity.activity_settings;
 import com.example.projets6.activity.logInActivity;
 
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +23,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button test=(Button) findViewById(R.id.test);
         test.setOnClickListener(v -> login());
+
+        ImageButton settings = (ImageButton) findViewById(R.id.btnSettings);
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSettings();
+            }
+        });
     }
+
+    private void openSettings() {
+        Intent intent = new Intent(this, activity_settings.class);
+        startActivity(intent);
+    }
+
     public void login(){
         Intent intent=new Intent(this,logInActivity.class);
         startActivity(intent);
---
+++ app/src/main/java/com/example/projets6/activity/activity_settings.java
@@ -0,0 +1,16 @@
+package com.example.projets6.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.projets6.R;
+
+public class activity_settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
