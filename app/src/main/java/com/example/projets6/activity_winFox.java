45b0100de55f1da01b3c06e7f7ad68fd21c52f9e
Ag031 <rudeau.agathe@gmail.com>
2022-06-13 20:21:13 +0200

    Page de Win du mode aventure xml

--- app/src/main/java/com/example/projets6/activity_winFox.java
+++ app/src/main/java/com/example/projets6/activity_winFox.java
@@ -2,7 +2,9 @@ package com.example.projets6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class activity_winFox extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class activity_winFox extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_win_fox);
+
+        ImageButton retour= (ImageButton) findViewById(R.id.retour);
+        retour.setOnClickListener(v -> retour());
+    }
+
+    private void retour() {
+        Intent intent = new Intent(this, com.example.projets6.activity.MainActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
     }
 }
+
